f7b7020cc951847827fd1dbdf7a9e3dc98cf621a
Antoine Pascual <74872926+Flashtheorie@users.noreply.github.com>
2022-03-29 14:32:31 +0200

    Indication si c'est rentable d'acheter une action aujourd'hui ou non

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ let dataMatin = [];
 let count = nbActions + 2;
 let bestValueMatin;
 
+// Déclaration des variables du soir :
+let dataSoir = [];
+let bestValueSoir;
+
+// Suivi des prix des actions le matin
 for (let i = 0; i < count; i++) {
     let actions = Math.floor((Math.random() * 1500 + 1));
     dataMatin.push(actions);
@@ -12,12 +17,37 @@ for (let i = 0; i < count; i++) {
     // Classe le tableau par ordre décroissant
     const sortDesc = (a, b) => b - a;
     dataMatin.sort(sortDesc);
-
     
     console.log(dataMatin);
+}
+bestValueMatin = dataMatin[0];
 
+// Suivi des prix des actions le soir
+
+for (let i = 0; i < count; i++) {
+    let actions = Math.floor((Math.random() * 1500 + 1));
+    dataSoir.push(actions);
+
+    // Classe le tableau par ordre décroissant
+    const sortDesc = (a, b) => b - a;
+    dataSoir.sort(sortDesc);
     
+    console.log(dataSoir);
 }
 
-bestValueMatin = dataMatin[0];
+bestValueSoir = dataSoir[0];
+
+
 console.log("La meilleur valeur du matin est " + bestValueMatin);
+console.log("La meilleur valeur du soir est " + bestValueSoir);
+
+
+if (bestValueMatin > bestValueSoir){
+    let difference = bestValueMatin - bestValueSoir;
+    console.log("Aujourd'hui, ce n'est pas intéressant d'acheter une action. Vous allez perdre " + difference +"€")
+}
+
+if (bestValueSoir > bestValueMatin){
+    let difference = bestValueSoir - bestValueMatin;
+    console.log("Aujourd'hui, vous allez gagner " + difference +"€")
+}
