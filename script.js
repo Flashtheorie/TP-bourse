da7065fa250309554b691b6f251a7b76751f0ab3
Antoine Pascual <74872926+Flashtheorie@users.noreply.github.com>
2022-03-29 14:02:36 +0200

    La boucle créant le tableau fonctionne

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 //let argent = prompt("Combien d'argent avez vous ?");
 let argent = 500;
 let nbActions = Math.floor((Math.random() * 100 + 2));
+let data = ['0'];
+let count = nbActions + 2;
 
-document.write(nbActions);
+
+for (let i = 0; i < count; i++) {
+    let actions = Math.floor((Math.random() * 1500 + 1));
+    data.push(actions);
+    console.log(data);
+    
+}
+
+
