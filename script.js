a6e314f3c6c49d6054423fcf9da563e370298790
Antoine Pascual <74872926+Flashtheorie@users.noreply.github.com>
2022-03-29 14:22:20 +0200

    Valeur des actions le matin

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 //let argent = prompt("Combien d'argent avez vous ?");
 let argent = 500;
 let nbActions = Math.floor((Math.random() * 100 + 2));
-let data = ['0'];
+let dataMatin = [];
 let count = nbActions + 2;
-
+let bestValueMatin;
 
 for (let i = 0; i < count; i++) {
     let actions = Math.floor((Math.random() * 1500 + 1));
-    data.push(actions);
-    console.log(data);
+    dataMatin.push(actions);
+
+    // Classe le tableau par ordre décroissant
+    const sortDesc = (a, b) => b - a;
+    dataMatin.sort(sortDesc);
+
+    
+    console.log(dataMatin);
+
     
 }
 
-
+bestValueMatin = dataMatin[0];
+console.log("La meilleur valeur du matin est " + bestValueMatin);
